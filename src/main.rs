a36262202d7dffe6078ff82785a73d61687bd23c
hahazexia <hahazexia@outlook.com>
2024-12-13 01:51:52 +0800

    feat: upscale opt

--- src/main.rs
+++ src/main.rs
@@ -70,8 +70,8 @@ enum DlType {
 // const RESET: &str = "\x1b[0m";   // 重置颜色
 // const YELLOW: &str = "\x1b[33m"; // 黄色
 
-// cargo run -- -u "C:/Users/hahaz/Downloads/伤追之人_单行本" -d "upscale"
-// cargo run -- -u "https://www.antbyw.com/plugin.php?id=jameson_manhua&c=index&a=bofang&kuid=182663" -d "juan"
+// cargo run -- -u "C:/Users/hahaz/Downloads/阴兽_单行本" -d "upscale"
+// cargo run -- -u "https://www.antbyw.com/plugin.php?id=jameson_manhua&c=index&a=bofang&kuid=142838" -d "juan"
 // cargo run -- -u "https://www.antbyw.com/plugin.php?id=jameson_manhua&a=read&kuid=152174&zjid=916038"
 
 const _UPSCAYL_MAC: &str = "/Applications/Upscayl.app/Contents/Resources/bin/upscayl-bin";
@@ -166,10 +166,6 @@ async fn handle_upscale (url: String) -> Result<bool> {
         a_number.cmp(&b_number)
     });
 
-    let bar = Arc::new(ProgressBar::new(dirs.len().try_into().unwrap()));
-    bar.set_style(ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {msg} {duration}")
-        .unwrap());
-
     for dir in dirs.iter() {
         let name = dir.get("name").unwrap().as_str().unwrap();
         let dir_path = dir.get("path").unwrap().as_str().unwrap();
@@ -177,12 +173,12 @@ async fn handle_upscale (url: String) -> Result<bool> {
         println!("name is {}", name);
 
         let new_dir_path = format!("{}/{}", output_path, name);
-        let new_dir_path_obj = Path::new(&new_dir_path);
-        if new_dir_path_obj.is_dir() {
-            println!("{}: {}", "dir already exist, continue next".green(), &new_dir_path);
-            bar.inc(1);
-            continue;
-        }
+        // let new_dir_path_obj = Path::new(&new_dir_path);
+        // if new_dir_path_obj.is_dir() {
+        //     println!("{}: {}", "dir already exist, continue next".green(), &new_dir_path);
+        //     bar.inc(1);
+        //     continue;
+        // }
         let _ = fs::create_dir_all(&new_dir_path);
         // upscayl-bin -i "输入目录" -o "输出目录" -c 50 -m "模型路径" -n "realesrgan-x4plus-anime" -f "png"
         // Usage: upscayl-bin -i infile -o outfile [options]...
@@ -217,40 +213,94 @@ async fn handle_upscale (url: String) -> Result<bool> {
             upscayl_model = _UPSCAYL_MODEL_MAC;
         }
 
-        let output = Command::new(upscayl)
-            .arg("-i")
-            .arg(dir_path)
-            .arg("-o")
-            .arg(&new_dir_path)
-            .arg("-s")
-            .arg("2")
-            .arg("-c")
-            .arg("50")
-            .arg("-m")
-            .arg(upscayl_model)
-            .arg("-n")
-            .arg("4x-DWTP-ds-esrgan-5")
-            .arg("-j")
-            .arg("1:1:1")
-            .arg("-f")
-            .arg("jpg")
-            .output()
-            .expect("Failed to execute command");
+        let mut image_files: Vec<PathBuf> = Vec::new();
+        for entry in fs::read_dir(dir_path)? {
+            let entry = entry?;
+            let path = entry.path();
 
-        // 处理输出
-        if output.status.success() {
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            println!("Output: {}", stdout);
-        } else {
-            let stderr = String::from_utf8_lossy(&output.stderr);
-            eprintln!("Error: {}", stderr);
+            if path.is_file() && path.extension().map_or(false, |ext| ext == "jpg") {
+                image_files.push(path);
+            }
         }
 
-        bar.inc(1);
+        image_files.sort_by_key(|path| {
+            path.file_name()
+                .and_then(|name| name.to_str())
+                .unwrap_or("")
+                .trim_end_matches(".jpg")
+                .parse::<u32>()
+                .unwrap_or(0)
+        });
+
+
+        let bar = Arc::new(ProgressBar::new(image_files.len().try_into().unwrap()));
+        bar.set_style(ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {msg} {duration}")
+            .unwrap());
+
+        for file in &image_files {
+            if let Some(temp_img_path) = file.to_str() {
+                if let Some(img_name) = file.file_name() {
+                    let output_img = format!("{}/{}", &new_dir_path, img_name.to_str().unwrap());
+                    if Path::new(&output_img).exists() {
+                        bar.inc(1);
+                        continue;
+                    }
+                    let _output = Command::new(upscayl)
+                        .arg("-i")
+                        .arg(&temp_img_path)
+                        .arg("-o")
+                        .arg(output_img)
+                        .arg("-s")
+                        .arg("2")
+                        .arg("-c")
+                        .arg("50")
+                        .arg("-m")
+                        .arg(upscayl_model)
+                        .arg("-n")
+                        .arg("4x-DWTP-ds-esrgan-5")
+                        .arg("-j")
+                        .arg("1:1:1")
+                        .arg("-f")
+                        .arg("jpg")
+                        .output()
+                        .expect("Failed to execute command");
+
+                    // if output.status.success() {
+                    //     let stdout = String::from_utf8_lossy(&output.stdout);
+                    //     println!("Output: {}", stdout);
+                    // } else {
+                    //     let stderr = String::from_utf8_lossy(&output.stderr);
+                    //     eprintln!("Error: {}", stderr);
+                    // }
+                    bar.inc(1);
+                    thread::sleep(Duration::from_secs(2));
+                }
+            }
+        }
+        let finish_text = format!("{} is done!", dirs.len());
+        bar.finish_with_message(finish_text.bright_blue().to_string());
+        // let output = Command::new(upscayl)
+        //     .arg("-i")
+        //     .arg(dir_path)
+        //     .arg("-o")
+        //     .arg(&new_dir_path)
+        //     .arg("-s")
+        //     .arg("2")
+        //     .arg("-c")
+        //     .arg("50")
+        //     .arg("-m")
+        //     .arg(upscayl_model)
+        //     .arg("-n")
+        //     .arg("4x-DWTP-ds-esrgan-5")
+        //     .arg("-j")
+        //     .arg("1:1:1")
+        //     .arg("-f")
+        //     .arg("jpg")
+        //     .output()
+        //     .expect("Failed to execute command");
+
         thread::sleep(Duration::from_secs(30));
     }
-    let finish_text = format!("{} is done!", dirs.len());
-    bar.finish_with_message(finish_text.bright_blue().to_string());
 
     Ok(true)
 }
